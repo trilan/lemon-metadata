317c86109c6377624c1d2215b35675e5e63c6874
Mike Yumatov <mike@yumatov.org>
2012-08-13 17:33:28 +0400

    Add initial migration

---
+++ metadata/migrations/0001_initial.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'Metadata'
+        db.create_table('metadata_metadata', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('url_path', self.gf('django.db.models.fields.CharField')(max_length=255, db_index=True)),
+            ('language', self.gf('django.db.models.fields.CharField')(default='en-us', max_length=10, db_index=True)),
+            ('title', self.gf('django.db.models.fields.CharField')(max_length=255, blank=True)),
+            ('title_extend', self.gf('django.db.models.fields.BooleanField')(default=True)),
+            ('keywords', self.gf('django.db.models.fields.TextField')(blank=True)),
+            ('description', self.gf('django.db.models.fields.TextField')(blank=True)),
+            ('priority', self.gf('django.db.models.fields.FloatField')(default=0.5, null=True, blank=True)),
+            ('changefreq', self.gf('django.db.models.fields.CharField')(default='M', max_length=1)),
+            ('lastmod', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime.now, null=True, blank=True)),
+            ('enabled', self.gf('django.db.models.fields.BooleanField')(default=True)),
+            ('content_type', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['contenttypes.ContentType'], null=True)),
+            ('object_id', self.gf('django.db.models.fields.PositiveIntegerField')(null=True)),
+        ))
+        db.send_create_signal('metadata', ['Metadata'])
+
+        # Adding M2M table for field sites on 'Metadata'
+        db.create_table('metadata_metadata_sites', (
+            ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
+            ('metadata', models.ForeignKey(orm['metadata.metadata'], null=False)),
+            ('site', models.ForeignKey(orm['sites.site'], null=False))
+        ))
+        db.create_unique('metadata_metadata_sites', ['metadata_id', 'site_id'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'Metadata'
+        db.delete_table('metadata_metadata')
+
+        # Removing M2M table for field sites on 'Metadata'
+        db.delete_table('metadata_metadata_sites')
+
+
+    models = {
+        'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'metadata.metadata': {
+            'Meta': {'object_name': 'Metadata'},
+            'changefreq': ('django.db.models.fields.CharField', [], {'default': "'M'", 'max_length': '1'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']", 'null': 'True'}),
+            'description': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'enabled': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'keywords': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'language': ('django.db.models.fields.CharField', [], {'default': "'en-us'", 'max_length': '10', 'db_index': 'True'}),
+            'lastmod': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'null': 'True', 'blank': 'True'}),
+            'object_id': ('django.db.models.fields.PositiveIntegerField', [], {'null': 'True'}),
+            'priority': ('django.db.models.fields.FloatField', [], {'default': '0.5', 'null': 'True', 'blank': 'True'}),
+            'sites': ('django.db.models.fields.related.ManyToManyField', [], {'blank': 'True', 'related_name': "'+'", 'null': 'True', 'symmetrical': 'False', 'to': "orm['sites.Site']"}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
+            'title_extend': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'url_path': ('django.db.models.fields.CharField', [], {'max_length': '255', 'db_index': 'True'})
+        },
+        'sites.site': {
+            'Meta': {'ordering': "('domain',)", 'object_name': 'Site', 'db_table': "'django_site'"},
+            'domain': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        }
+    }
+
+    complete_apps = ['metadata']
