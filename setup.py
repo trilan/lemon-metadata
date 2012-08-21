0ddb8f2b057eb5fee840534a95498abab60fa9de
Mike Yumatov <mike@yumatov.org>
2012-08-21 17:12:35 +0400

    Bump version number

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read(filename):
 
 setup(
     name='lemon-metadata',
-    version='0.1',
+    version='0.1.1',
     license='ISC',
     description='A Django app to manage HTML metadata',
     long_description=read('README.rst'),
