dd9529fedd20f120c8ad390d358aa10cc9fbc491
Mike Yumatov <mike@yumatov.org>
2012-08-21 10:08:06 +0400

    Fix add form view for models with metadata

--- metadata/templatetags/metadata.py
+++ metadata/templatetags/metadata.py
@@ -1,4 +1,6 @@
+from django.db.models import Model
 from django.template import Library
+
 from ..models import Metadata
 
 
@@ -7,6 +9,8 @@ register = Library()
 
 @register.assignment_tag
 def get_metadata_for(obj):
+    if not isinstance(obj, Model):
+        return None
     try:
         return Metadata.objects.get_for_content_object(obj)
     except Metadata.DoesNotExist:
